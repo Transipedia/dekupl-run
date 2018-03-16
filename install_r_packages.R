9d7852a7fea8f593251547b6457b07ea27f81a52
Jérôme Audoux <jerome.audoux@gmail.com>
2018-03-16 10:13:42 +0100

    Change default R repo

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,7 +1,7 @@
-repository <- "https://cloud.r-project.org"
-install.packages("RColorBrewer", repos=repository)
-install.packages("pheatmap", repos=repository)
-install.packages("foreach", repos=repository)
-install.packages("doParallel", repos=repository)
+options(repos='http://cran.rstudio.com/')
+install.packages("RColorBrewer")
+install.packages("pheatmap")
+install.packages("foreach")
+install.packages("doParallel")
 source("https://bioconductor.org/biocLite.R")
 biocLite("DESeq2")
